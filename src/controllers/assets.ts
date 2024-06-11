d3659bd5cd6825070d886d8bf75f9e1908f87f85
esolar07 <esolar07@gmail.com>
2024-06-11 14:59:47 -0400

    - added functionality to create asset folders for restaurants - also add upload image functionality

--- src/controllers/assets.ts
+++ src/controllers/assets.ts
@@ -1,16 +1,31 @@
 import {v2 as cloudinary} from 'cloudinary';
 
 
-export const uploadImage = async () => {
+export const uploadImage = async (req, res) => {
     const options = {
+        public_id: "abc test image",
+        folder: "restaurant/abc",
         use_filename: true,
         unique_filename: false,
         overwrite: true,
     };
     try {
-        const result = await cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg", options);
-        return result.public_id;
+        await cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg", options)
+            .then((result) => {
+                return res.json({ message: 'Restaurant asset uploaded successfully', data: result});
+            });
     } catch (error) {
         console.error(error);
     }
 };
+
+export const createFolder = async (req, res) => {
+    try {
+        await cloudinary.api.create_folder("restaurant/abc")
+            .then((result) => {
+                return res.json({ message: 'Restaurant asset folder created successfully', result});
+            });
+    } catch (error) {
+        console.error(error);
+    }
+}
--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { registerNewUser } from "./controllers/users";
 import { createRestaurant } from "./controllers/restaurant"
 import { authenticate } from './controllers/auth';
-import {uploadImage} from "./controllers/assets";
+import {uploadImage, createFolder} from "./controllers/assets";
 
 const router = Router()
 router.get('/home', (req, res)=> {
@@ -11,6 +11,7 @@ router.get('/home', (req, res)=> {
 router.post('/register', registerNewUser);
 router.post('/restaurant/create', createRestaurant);
 router.post('/authenticate', authenticate);
-router.post('/image/upload', uploadImage)
+router.post('/assets/upload', uploadImage)
+router.post('/assets/folder/create', createFolder)
 
 export default router
