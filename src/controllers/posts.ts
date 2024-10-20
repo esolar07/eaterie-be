8b66d6a4840cbfa12d257f4fef528d4340cc7df4
esolar07 <esolar07@gmail.com>
2024-10-20 00:15:02 -0400

    working on build pst table hadnlers

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -2,10 +2,21 @@ import prisma from "../db";
 import {createJwt, hashPassword} from "../modules/auth";
 
 export const createPost = async (req, res) => {
+    const { postTitle, postImage } = req.body,
+           userId = req.headers.authorization;
+    const restaurant = prisma.restaurant.findFirst({
+        where: {
+            userId: userId
+        }
+    })
+    res.json(userId);
+
     // try {
     //     const post = await prisma.post.create({
     //         data: {
-    //             postTitle:req.body.postTitle,
+    //             postTitle: postTitle,
+    //             postImage: postImage,
+    //             restaurantId: restaurant.
     //         }
     //     })
     //     await res.json({})
--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import { registerNewUser, signIn } from "./controllers/users";
 import { createRestaurant } from "./controllers/restaurant"
 import {uploadImage, createAssetFolders} from "./controllers/assets";
 import { verifyToken } from "./modules/auth";
+import {createPost} from "./controllers/posts";
 
 const router = Router()
 router.get('/home', (req, res)=> {
@@ -12,7 +13,7 @@ router.post('/register', registerNewUser);
 router.get('/user')
 router.post('/register/restaurant', verifyToken, createRestaurant);
 router.post('/authenticate', signIn);
-router.post('/post', uploadImage)
+router.post('/post', verifyToken, createPost)
 router.get('/post')
 
 export default router
